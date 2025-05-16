30232de1571dc92cd88d0f2e6cacab659a95e56f
ErenJeagerX <manjiroisback@gmail.com>
2025-05-16 09:22:07 -0700

    second commit

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Habnest | Admin Dashboard</title>
-    <link rel="stylesheet" href="../assets/css/style.css?v=2">
-    <link rel="stylesheet" href="../icons/fontawesome/css/all.css">
-    <script src="../assets/JS/dashboard-admin.js?v=2" defer type="module"></script>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="../assets/JS/dashboard-admin.js" defer type="module"></script>
 </head>
 <body>
     <div class="wrapper-dashboard">
