7f1321b7948dffc6b1ac371778a96bf6f29516ff
ErenJeagerX <manjiroisback@gmail.com>
2025-05-18 02:18:57 -0700

    .

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,7 +7,6 @@
     <link rel="shortcut icon" href="../assets/imgs/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="../assets/css/style.css<?php echo "?v=" . time()?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../../fontawesome/css/all.css">
     <script src="../assets/JS/dashboard-admin.js?v=<?php echo time() ?>" defer type="module"></script>
 </head>
 <body>
