dbde667779eba36b6bb70f18731588f2cde88f06
ErenJeagerX <manjiroisback@gmail.com>
2025-05-24 07:32:58 -0700

    .

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="../icon/image (7).jpg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./CSS/style.css" />
+    <title>Habnest</title>
+  </head>
+  <body>
+    <section class="main">
+      <!-- navigation starts here -->
+      <nav>
+        <div class="container-fluid">
+          <ul class="d-flex gap-3 list-unstyled text-secondary pt-3">
+            <li>
+              <a href="#"><img src="icon/image (7).jpg" alt="" /></a>
+            </li>
+            <li><a href="#">EthioRent</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="./login.php">Login</a></li>
+          </ul>
+        </div>
+      </nav>
+      <!-- navigation ends here -->
+
+      <!-- section-1 starts here -->
+      <section class="sec-1">
+        <div class="pxy">
+          <div class="discover p-5">
+            <div class="habnest-txt mb-4">Welcome to Habnest Rental</div>
+            <div class="display-5 mb-3">Discover Your Next Rental</div>
+
+            <div class="mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+              atrerum.
+            </div>
+
+            <div class="sharp-border">Fixed bluh bluh</div>
+            <div class="p-4">
+              <div class="mb-4">Lorem ipsum dolor sit amet</div>
+              <div class="mb-3">Lorem ipsum dolor sit amet</div>
+            </div>
+            <div>
+              <button class="contact-btn">Contact Us</button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- section-1 ends here -->
+
+      <!-- section-2 starts here -->
+
+      <section class="sec-2">
+        <div class="txts text-center">
+          <div class="txt-1">Some text here</div>
+          <div class="txt-2 display-5">Our Mission</div>
+          <div class="txt-3 mb-5">another text here</div>
+        </div>
+
+        <div class="mission-wrapper">
+          <div class="container row justify-content-center lndLord">
+            <div class="lnd four-boxes col-4 col-md-12 p-">
+              <div class="fs-4 p-2">Landlord Tool</div>
+              <div class="mb-3">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+                totam officia ipsam doloribus aliquid quisquam in consequatur
+              </div>
+              <button class="detail">Details</button>
+            </div>
+
+            <div class="rental four-boxes col-4 col-md-12">
+              <div class="fs-4 p-2">Rental Search</div>
+              <div class="mb-3">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+                totam officia ipsam doloribus aliquid quisquam in consequatur
+              </div>
+              <button class="detail">Details</button>
+            </div>
+          </div>
+
+          <div class="container row justify-content-center renterex">
+            <div class="rentex four-boxes col-4 col-md-12">
+              <div class="fs-4 p-2">Renter Experience</div>
+              <div class="mb-3">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+                totam officia ipsam doloribus aliquid quisquam in consequatur
+              </div>
+              <button class="detail">Details</button>
+            </div>
+
+            <div class="trstLord four-boxes col-4 col-md-12">
+              <div class="fs-4 p-2">Trusted Landlord</div>
+              <div class="mb-3">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+                totam officia ipsam doloribus aliquid quisquam in consequatur
+              </div>
+              <button class="detail">Details</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- section-2 starts here -->
+
+      <!-- section-3 starts here -->
+
+      <section class="sec-3">
+        <div class="text-center">
+          <div>Text here</div>
+          <div class="fs-1">Our Advantages</div>
+          <div class="mb-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </div>
+        </div>
+        <div
+          class="container d-flex flex-wrap justify-content-center first-img-wrapper"
+        >
+          <div class="">
+            <div class="first-img four-imges"></div>
+            <div class="p-3">textttttttt</div>
+          </div>
+
+          <div class="">
+            <div class="second-img four-imges"></div>
+            <div class="p-3 mb-4">texttttttt</div>
+          </div>
+        </div>
+
+        <div
+          class="container d-flex flex-wrap mb-4 justify-content-center third-img-wrapper"
+        >
+          <div>
+            <div class="third-img four-imges"></div>
+            <div class="p-3">texttt</div>
+          </div>
+
+          <div>
+            <div class="forth-img four-imges"></div>
+            <div class="p-3">textttt</div>
+          </div>
+        </div>
+
+        <div class="text-center">
+          <div class="mb-5">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </div>
+        </div>
+
+        <div
+          class="container d-flex flex-wrap justify-content-center mb-5 img-wrapper"
+        >
+          <div>
+            <div class="img-1 three-img"></div>
+            <div class="p-3">textaaaaaaaa</div>
+          </div>
+
+          <div>
+            <div class="img-2 three-img"></div>
+            <div class="p-3">textaaaaaaaa</div>
+          </div>
+
+          <div>
+            <div class="img-3 three-img"></div>
+            <div class="p-3">textaaaaaaaa</div>
+          </div>
+        </div>
+      </section>
+      <!-- section-3 ends here -->
+
+      <!-- footer -->
+      <footer class="bg-dark text-secondary">
+        <div class="ftr">@copyright-2025</div>
+      </footer>
+      <!-- ----------------- -->
+    </section>
+  </body>
+</html>
