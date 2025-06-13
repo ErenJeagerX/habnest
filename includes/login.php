47d2d6cd021a78a07bfaec52dda2259a6f1c8f1e
Abresh512 <abrahamtadesse5123@gmail.com>
2025-06-13 15:32:37 +0300

    navbar

---
+++ includes/login.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+// database connection
+include('db.php');
+
+
+$passwd_error = $usr_error = $message = "";
+// Check if the form has been submitted
+if(isset($_POST['submit'])) {
+    $username = trim($_POST['username']);
+    $pwd = trim($_POST['pwd']);
+
+    // check if the fields are empty or not...
+    if (empty($username)) {
+        $message = '<div class="alert alert-warning" role="alert">Please enter your username.</div>';
+    } elseif (empty($pwd)) {
+        $message = '<div class="alert alert-warning" role="alert">Please enter your password.</div>';
+    } else {
+        $sql = "SELECT * FROM users WHERE username = ?";
+
+        // Prepare the statement
+        if ($stmt = $conn->prepare($sql)) {
+            $stmt->bind_param("s", $username);
+
+            $stmt->execute();
+
+            // Get the result set
+            $result = $stmt->get_result();
+
+            // Check if a user with that username exists
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $db_hashed_pwd = $row['pwd']; 
+                if (password_verify($pwd, $db_hashed_pwd)) { 
+                    // Password is correct
+                    $_SESSION['name'] = $row['first_name'];
+                    $_SESSION['id'] = $row['id'];
+                    
+                    //cookies
+                    if(isset($_POST['remember_me'])){
+                        $remr = $_POST['remember_me'];
+
+                        setcookie("usr_cookie", $username, time() + 86400 * 30, '/');
+                        setcookie("remr_cookie", $remr, time() + 86400 * 30, '/');
+
+                    }else{
+                        if(isset($_COOKIE['usr_cookie'])){
+                            setcookie("usr_cookie", $username, time() - 86400 * 30, '/');
+                        }
+                        if(isset($_COOKIE['remr_cookie'])){
+                            setcookie("remr_cookie", $remr, time() - 86400 * 30, '/');
+                        }
+                       
+                }
+
+                //cookies end here
+                    // redirecting the user to the dashboard (successful login)
+                    if($row['role'] == 'landlord'){
+                        header("Location: ./landlord/dashboard.php");
+                        exit();
+                    } elseif($row['role'] == 'admin') {
+                        header("Location: ./admin/dashboard.php");
+                        exit();
+                    }
+
+                    // if there is an error ....
+                } else {
+                    $passwd_error = "Invalid Password";
+                }
+            } else {
+                $usr_error = "Invalid Username";
+            }
+
+            $stmt->close(); 
+        } else {
+            $message = 'Database Failed.';
+        }
+    }
+}
+
+?>
---
+++ includes/navbar.php
@@ -0,0 +1,24 @@
+<?php
+include('login.php');
+?>
+  <nav>
+        <input type="checkbox" id="nav-toggle" class="nav-toggle" />
+        <label for="nav-toggle" class="nav-toggle-label">
+          <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </label>
+        <ul class="align-items-center">
+          <li>
+            <a href="index.php"><img src="./assets/imgs/homepage_logo.png" alt="" /></a>
+          </li>
+            
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="./properties.php">Properties</a></li>
+                <li><a href="./login.php">Login</a></li>
+          
+        </ul>
+      </nav>
